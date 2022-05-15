fbe8a929e46ea2ad19b56fe93f23650c49bda457
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-05-15 18:25:29 +0200

    Iimplemented functionalities for Renovation

---
+++ Project/Hospital/Model/Renovation.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Model
+{
+    public class Renovation
+    {
+        public Renovation(Room room, Appointment appointment, RenovationType renovationType)
+        {
+            Room = room;
+            Appointment = appointment;
+            RenovationType = renovationType;
+        }
+        public Room Room { get; set; }
+        public RenovationType RenovationType { get; set; }
+        public Appointment Appointment { get; set; }
+        public bool ShouldSerializeRoom()
+        {
+            if (this.Room != null)
+                this.Room.serialize = false;
+            return true;
+        }
+
+        public bool ShouldSerializeAppointment()
+        {
+            if (this.Appointment != null)
+                this.Appointment.serialize = false;
+            return true;
+        }
+    }
+}
---
+++ Project/Hospital/Service/RenovationService.cs
@@ -0,0 +1,66 @@
+﻿using Hospital.Model;
+using Hospital.Repository;
+using Model;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Service
+{
+    public class RenovationService
+    {
+        public AppointmentService appointmentService;
+        public RenovationService(RenovationRepository renovationRepository,AppointmentService appointmentService)
+        {
+            this.renovationRepository = renovationRepository;
+            this.appointmentService = appointmentService;
+        }
+        public List<Renovation> GetAll()
+        {
+            return renovationRepository.GetAll();
+        }
+
+        public bool Delete(Renovation renovation)
+        {
+            appointmentService.DeleteAppointment(renovation.Appointment.Id);
+            return renovationRepository.Delete(renovation);
+        }
+
+        public bool Edit(Renovation renovation)
+        {
+            return renovationRepository.Edit(renovation);
+        }
+
+        public bool Create(Renovation renovation)
+        {
+            if (renovation.Appointment.Room == null) 
+            {
+                return false;
+            }
+
+            if (renovation.RenovationType == RenovationType.Merger) 
+            {
+                int ids = appointmentService.GetAll().Count() == 0 ? 0 : appointmentService.GetAll().Max(Appointment => Appointment.Id);
+                if (renovation.Room == null) 
+                {
+                    appointmentService.DeleteAppointment(renovation.Appointment.Id);
+                    return false;
+                }
+        
+
+                if (!appointmentService.CreateRenovation(++ids, renovation.Appointment.StartTime, renovation.Appointment.EndTime, 0, true, AppointmentType.renovationAppointment, null, null, renovation.Room)) 
+                {
+                    appointmentService.DeleteAppointment(renovation.Appointment.Id);
+                    return false;
+                }       
+            }
+
+            return renovationRepository.Create(renovation);
+        }
+
+        public Repository.RenovationRepository renovationRepository;
+    }
+}
